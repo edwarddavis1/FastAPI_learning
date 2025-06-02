7dea67193bb347d36e97193c3913c11d2e05af09
Ed <edwarddavis@hotmail.co.uk>
2025-06-02 14:44:52 +0100

    added http tests

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 app = FastAPI()
+
+origins = ["http://127.0.0.1:3000"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Mount static files directory to serve HTML, CSS, JS files
 app.mount("/static", StaticFiles(directory="static"), name="static")
